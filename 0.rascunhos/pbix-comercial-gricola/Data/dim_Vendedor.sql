d2619f35351076875ca67a1d84f12c9300851ec2
GusttavoMartins <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>
2026-07-02 15:59:26 -0300

    Remove rascunhos do repositorio

--- 0.rascunhos/pbix-comercial-gricola/Data/dim_Vendedor.sql
+++
@@ -1,9 +0,0 @@
-select
-Vendedor.id
-,Vendedor.descricao as descricao_vendedor
-,Supervisor.descricao as descricao_supervisor
-,Supervisor.gerente_id as gerente_id
-, Supervisor.gerente_descricao as descricao_gerente
-from dbo.Vendedor
-join dbo.Supervisor
-	on (Vendedor.supervisor_id = Supervisor.id)
